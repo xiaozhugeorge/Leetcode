0b81b218e1023324de8d0056cbbd554dba434456
xiaozhugeorge <zhuxiaogeorge@gmail.com>
2018-08-09 15:34:33 -0400

    Update Solution.sql

--- SQL/Join/176. Second Highest Salary/Solution.sql
+++ SQL/Join/176. Second Highest Salary/Solution.sql
@@ -1,1 +1,9 @@
+MSSQL:
 
+Select 
+   ISNULL(
+    (SELECT distinct salary 
+    FROM Employee
+    ORDER BY Salary desc
+    OFFSET 1 ROWS
+    FETCH NEXT 1 ROWS ONLY),null) AS SecondHighestSalary;
